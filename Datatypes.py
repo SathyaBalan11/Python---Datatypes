05bd5692369756a325b4b7773068a1f5d66fc094
SathyaBalan11 <sathyarishik@gmail.com>
2025-06-12 16:22:13 +0530

    Datatypes

---
+++ Datatypes.py
@@ -0,0 +1,77 @@
+#1 String
+quote = "Good Things Take Time"
+print("Quote:", quote)
+print(type(quote))
+#output
+
+
+#2 List
+numbers = ["2","1","0","1"]
+print("Numbers:", numbers)
+print(type(numbers))
+#output
+
+
+#3 Tuple
+colors = ("black","white","pink","black")
+print("Colors:", colors)
+print(type(colors))
+#output
+
+
+#4 Set
+veggies = {"carrot","potato","beans","carrot"}
+print("Veggies:", veggies)
+print(type(veggies))
+#output
+
+
+#5 Integer
+number = 11
+print("Number:", number)
+print(type(number))
+#output
+
+
+#6 Float
+price = 20.5
+print("Price:", price)
+print(type(price))
+#output
+
+
+#7 Complex
+number = 3+4j
+print("Number:", number)
+print(type(number))
+#output
+
+
+#8 Boolean
+children = True
+childrens = False
+print("Children:", children)
+print("Childrens:", childrens)
+print(type(children))
+#output
+
+
+#9 Dictionary
+candidate = {"name": "Shiva", "course": "Computer Science"}
+print("Candidate Details:", candidate)
+print(type(candidate))
+#output
+
+
+#10 Range
+numbers = range(5)
+print("Range from 1 to 4:", (numbers))
+print(type(numbers))
+#output
+
+
+#11 None
+result = None
+print("Result:", result)
+print(type(result))
+#output
